AOSP-Krypton/frameworks_base

[SQUASH]base: Introduce PixelPropsUtils
That will spoof build fingerprints on some g00gle apps

* Also will enable some cool stuffs like:
  - Dynamic navbar on GBoard
  - SafetyHub

and others.

Thanks to @kdrag0n for the original idea at https://github.com/ProtonAOSP/android_frameworks_base/commit/5a54bfd846c7a26ba4820a361a6fec779edf8c5a

Change-Id: I1078e7402833fec77edb751070c5144d08c85b6c
Signed-off-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
Co-authored-by: Danny Lin <danny@kdrag0n.dev>

PixelPropsUtils: Override fingerprint for SettingsIntelligence

Change-Id: Ia839345da18dde46bfd1acb77b42e0c47d28c8ac

PixelPropsUtils: Cleanup apps

Change-Id: Ida149c01cb7bf09a4275e7e436ce99ffc94a5bf0
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

PixelPropsUtils: bring back wallpaper collection

Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
Change-Id: Ia3c11cc377107ed856fc9d9594aad45bf5a49185
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

PixelPropsUtils: Change props to crosshatch

Change-Id: I577ceed8b54311e4255a28573f99d1a53252466a
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

PixelPropsUtils: Set pixel 5 props for specific apps

Change-Id: I36d1bc53679ea6d21cc304420b12d7500b093a2a
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

PixelPropsUtils: Switch back to Pixel 5 props for all apps

Change-Id: I54e2c16aee37d3d18f9b7a08e2b1d09b94e8e599
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

PixelPropsUtils: Set redfin build fingerprint

Change-Id: Ib819a2f3494cffc10590638ac260cebfd8cffe79
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

PixelPropsUtils: Set device model to Pixel 3 XL for Google

Credits to @kdrag0n. Based on https://github.com/ProtonAOSP/android_frameworks_base/commit/5078079773c75b67f883236eb60a4e520f71b43d

Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

PixelPropsUtils: Update packages

Change-Id: I8c0c55e258bdf767050f07bd83e3d1df44ea98c2
Co-authored-by: jhenrique09 <jhenrique09.mcz@ho

core/java/com/android/internal/os/Zygote.java
@@ -47,6 +47,8 @@ import java.io.DataOutputStream;
 import java.io.FileDescriptor;
 import java.io.IOException;
 
+import com.android.internal.util.krypton.PixelPropsUtils;
+
 /** @hide */
 public final class Zygote {
     /*
@@ -883,6 +885,9 @@ public final class Zygote {
         } else {
             Log.w(loggingTag, "Unable to set package name.");
         }
+
+        // Set pixel props
+        PixelPropsUtils.setProps(args.mPackageName);
     }
 
     private static final String USAP_ERROR_PREFIX = "Invalid command to USAP: ";

core/java/com/android/internal/util/krypton/PixelPropsUtils.java
@@ -1,0 +1,147 @@
+/*
+ * Copyright (C) 2020 The Pixel Experience Project
+ *               2021 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.internal.util.krypton;
+
+import android.os.Build;
+import android.util.Log;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+
+public final class PixelPropsUtils {
+
+    private static final String TAG = "PixelPropsUtils";
+    private static final boolean DEBUG = false;
+
+    private static final Map<String, String> redfinProps = Map.of(
+        "BRAND", "google",
+        "MANUFACTURER", "Google",
+        "DEVICE", "redfin",
+        "PRODUCT", "redfin",
+        "MODEL", "Pixel 5",
+        "FINGERPRINT", "google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys"
+    );
+
+    private static final Map<String, String> marlinProps = Map.of(
+        "BRAND", "google",
+        "MANUFACTURER", "Google",
+        "DEVICE", "marlin",
+        "PRODUCT", "marlin",
+        "MODEL", "Pixel XL",
+        "FINGERPRINT", "google/marlin/marlin:10/QP1A.191005.007.A3/5972272:user/release-keys"
+    );
+
+    private static final Map<String, String> walleyeProps = Map.of(
+        "BRAND", "google",
+        "MANUFACTURER", "Google",
+        "DEVICE", "walleye",
+        "PRODUCT", "walleye",
+        "MODEL", "Pixel 2",
+        "FINGERPRINT", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
+    );
+
+    private static final Map<String, String> crosshatchProps = Map.of(
+        "BRAND", "google",
+        "MANUFACTURER", "Google",
+        "DEVICE", "crosshatch",
+        "PRODUCT", "crosshatch",
+        "MODEL", "Pixel 3 XL",
+        "FINGERPRINT", "google/crosshatch/crosshatch:11/RQ3A.211001.001/7641976:user/release-keys"
+    );
+
+    private static final List<String> packagesToChange = List.of(
+        "com.android.vending",
+        "com.breel.wallpapers20",
+        "com.google.android.apps.customization.pixel",
+        "com.google.android.apps.fitness",
+        "com.google.android.apps.maps",
+        "com.google.android.apps.nexuslauncher",
+        "com.google.android.apps.recorder",
+        "com.google.android.apps.safetyhub",
+        "com.google.android.apps.subscriptions.red",
+        "com.google.android.apps.tachyon",
+        "com.google.android.apps.turbo",
+        "com.google.android.apps.turboadapter",
+        "com.google.android.apps.wallpaper",
+        "com.google.android.apps.wallpaper.pixel",
+        "com.google.android.as",
+        "com.google.android.dialer",
+        "com.google.android.gms.location.history",
+        "com.google.android.inputmethod.latin",
+        "com.google.android.soundpicker",
+        "com.google.pixel.dynamicwallpapers",
+        "com.google.pixel.livewallpaper"
+    );
+
+    private static final List<String> packagesToChangePixelXL = List.of(
+        "com.google.android.apps.photos",
+        "com.samsung.accessory.berrymgr",
+        "com.samsung.accessory.fridaymgr",
+        "com.samsung.accessory.neobeanmgr",
+        "com.samsung.android.app.watchmanager",
+        "com.samsung.android.geargplugin",
+        "com.samsung.android.gearnplugin",
+        "com.samsung.android.modenplugin",
+        "com.samsung.android.neatplugin",
+        "com.samsung.android.waterplugin"
+    );
+
+    private static final List<String> packagesToChangePixel2 = List.of(
+        "com.google.android.gms"
+    );
+
+    private static final List<String> packagesToChangePixel3XL = List.of(
+        "com.google.android.googlequicksearchbox"
+    );
+
+    public static void setProps(String packageName) {
+        if (packageName == null) {
+            return;
+        }
+        if (DEBUG) {
+            Log.d(TAG, "Package = " + packageName);
+        }
+        if (packagesToChange.contains(packageName)) {
+            redfinProps.forEach(PixelPropsUtils::setPropValue);
+        } else if (packagesToChangePixelXL.contains(packageName)) {
+            marlinProps.forEach(PixelPropsUtils::setPropValue);
+        } else if (packagesToChangePixel2.contains(packageName)) {
+            walleyeProps.forEach(PixelPropsUtils::setPropValue);
+        } else if (packagesToChangePixel3XL.contains(packageName)) {
+            crosshatchProps.forEach(PixelPropsUtils::setPropValue);
+        }
+        // Set proper indexing fingerprint
+        if (packageName.equals("com.google.android.settings.intelligence")) {
+            setPropValue("FINGERPRINT", Build.DATE);
+        }
+    }
+
+    private static void setPropValue(String key, String value) {
+        try {
+            if (DEBUG) {
+                Log.d(TAG, "Setting prop " + key + " to " + value);
+            }
+            final Field field = Build.class.getDeclaredField(key);
+            field.setAccessible(true);
+            field.set(null, value);
+            field.setAccessible(false);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            Log.e(TAG, "Failed to set prop " + key, e);
+        }
+    }
+}

yaap/frameworks_base_old

core/java/com/android/internal/os/Zygote.java
@@ -47,6 +47,8 @@ import java.io.DataOutputStream;
 import java.io.FileDescriptor;
 import java.io.IOException;
 
+import com.android.internal.util.yaap.PixelPropsUtils;
+
 /** @hide */
 public final class Zygote {
     /*
@@ -883,6 +885,9 @@ public final class Zygote {
         } else {
             Log.w(loggingTag, "Unable to set package name.");
         }
+
+        // Set pixel props
+        PixelPropsUtils.setProps(args.mPackageName);
     }
 
     private static final String USAP_ERROR_PREFIX = "Invalid command to USAP: ";

core/java/com/android/internal/util/yaap/PixelPropsUtils.java
@@ -1,0 +1,147 @@
+/*
+ * Copyright (C) 2020 The Pixel Experience Project
+ *               2021 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.internal.util.yaap;
+
+import android.os.Build;
+import android.util.Log;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+
+public final class PixelPropsUtils {
+
+    private static final String TAG = "PixelPropsUtils";
+    private static final boolean DEBUG = false;
+
+    private static final Map<String, String> redfinProps = Map.of(
+        "BRAND", "google",
+        "MANUFACTURER", "Google",
+        "DEVICE", "redfin",
+        "PRODUCT", "redfin",
+        "MODEL", "Pixel 5",
+        "FINGERPRINT", "google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys"
+    );
+
+    private static final Map<String, String> marlinProps = Map.of(
+        "BRAND", "google",
+        "MANUFACTURER", "Google",
+        "DEVICE", "marlin",
+        "PRODUCT", "marlin",
+        "MODEL", "Pixel XL",
+        "FINGERPRINT", "google/marlin/marlin:10/QP1A.191005.007.A3/5972272:user/release-keys"
+    );
+
+    private static final Map<String, String> walleyeProps = Map.of(
+        "BRAND", "google",
+        "MANUFACTURER", "Google",
+        "DEVICE", "walleye",
+        "PRODUCT", "walleye",
+        "MODEL", "Pixel 2",
+        "FINGERPRINT", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
+    );
+
+    private static final Map<String, String> crosshatchProps = Map.of(
+        "BRAND", "google",
+        "MANUFACTURER", "Google",
+        "DEVICE", "crosshatch",
+        "PRODUCT", "crosshatch",
+        "MODEL", "Pixel 3 XL",
+        "FINGERPRINT", "google/crosshatch/crosshatch:11/RQ3A.211001.001/7641976:user/release-keys"
+    );
+
+    private static final List<String> packagesToChange = List.of(
+        "com.android.vending",
+        "com.breel.wallpapers20",
+        "com.google.android.apps.customization.pixel",
+        "com.google.android.apps.fitness",
+        "com.google.android.apps.maps",
+        "com.google.android.apps.nexuslauncher",
+        "com.google.android.apps.recorder",
+        "com.google.android.apps.safetyhub",
+        "com.google.android.apps.subscriptions.red",
+        "com.google.android.apps.tachyon",
+        "com.google.android.apps.turbo",
+        "com.google.android.apps.turboadapter",
+        "com.google.android.apps.wallpaper",
+        "com.google.android.apps.wallpaper.pixel",
+        "com.google.android.as",
+        "com.google.android.dialer",
+        "com.google.android.gms.location.history",
+        "com.google.android.inputmethod.latin",
+        "com.google.android.soundpicker",
+        "com.google.pixel.dynamicwallpapers",
+        "com.google.pixel.livewallpaper"
+    );
+
+    private static final List<String> packagesToChangePixelXL = List.of(
+        "com.google.android.apps.photos",
+        "com.samsung.accessory.berrymgr",
+        "com.samsung.accessory.fridaymgr",
+        "com.samsung.accessory.neobeanmgr",
+        "com.samsung.android.app.watchmanager",
+        "com.samsung.android.geargplugin",
+        "com.samsung.android.gearnplugin",
+        "com.samsung.android.modenplugin",
+        "com.samsung.android.neatplugin",
+        "com.samsung.android.waterplugin"
+    );
+
+    private static final List<String> packagesToChangePixel2 = List.of(
+        "com.google.android.gms"
+    );
+
+    private static final List<String> packagesToChangePixel3XL = List.of(
+        "com.google.android.googlequicksearchbox"
+    );
+
+    public static void setProps(String packageName) {
+        if (packageName == null) {
+            return;
+        }
+        if (DEBUG) {
+            Log.d(TAG, "Package = " + packageName);
+        }
+        if (packagesToChange.contains(packageName)) {
+            redfinProps.forEach(PixelPropsUtils::setPropValue);
+        } else if (packagesToChangePixelXL.contains(packageName)) {
+            marlinProps.forEach(PixelPropsUtils::setPropValue);
+        } else if (packagesToChangePixel2.contains(packageName)) {
+            walleyeProps.forEach(PixelPropsUtils::setPropValue);
+        } else if (packagesToChangePixel3XL.contains(packageName)) {
+            crosshatchProps.forEach(PixelPropsUtils::setPropValue);
+        }
+        // Set proper indexing fingerprint
+        if (packageName.equals("com.google.android.settings.intelligence")) {
+            setPropValue("FINGERPRINT", Build.DATE);
+        }
+    }
+
+    private static void setPropValue(String key, String value) {
+        try {
+            if (DEBUG) {
+                Log.d(TAG, "Setting prop " + key + " to " + value);
+            }
+            final Field field = Build.class.getDeclaredField(key);
+            field.setAccessible(true);
+            field.set(null, value);
+            field.setAccessible(false);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            Log.e(TAG, "Failed to set prop " + key, e);
+        }
+    }
+}